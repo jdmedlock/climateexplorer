1be10daa6f70fd5935c0329b3baf6009694f6ecb
jdmedlock <jdmedlock@gmail.com>
2018-12-24 19:27:29 -0600

    Feature: Add basic Apollo client-side framework
    Add basic Apollo client-side framework
    
    Resolves: N/a
    See also: N/a

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,30 @@
+import { ApolloClient } from "apollo-client";
+import { ApolloProvider } from 'react-apollo';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  cache,
+  link: new HttpLink({
+    uri: 'http://localhost:4000/graphql',
+    headers: {
+      authorization: localStorage.getItem('token'),
+      'client-name': 'Climate Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
