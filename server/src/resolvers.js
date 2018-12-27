2326671dcc7a61497564184c79fd733d78cef4c7
jdmedlock <jdmedlock@gmail.com>
2018-12-26 18:55:40 -0600

    Feature: Add `login` mutation skeleton
    Add `login` mutation skeleton
    
    Resolves: N/a
    See also: N/a

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,4 +3,9 @@ module.exports = {
     me: async (_, __, { dataSources }) =>
       dataSources.user,
   },
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      return null;
+    },
+  },
 };
